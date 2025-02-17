c5857e078252da7fdcad23e01d60896d6667058e
mohammadAlayd <mohammadalaydi6@gmail.com>
2025-02-17 20:14:17 +0200

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 import React, { Suspense, lazy } from 'react';
+import Navbar from './components/navbar';
 export default function Home() {
   return (
     <>
+   <Navbar />
+
     </>
 
   );
---
+++ app/pages/console/nintendo.tsx
@@ -0,0 +1,8 @@
+export default function Nintendo() {
+    return (
+      <div className="p-8">
+        <h1 className="text-3xl font-bold">Gaming Gear</h1>
+        <p>Explore our latest gaming gear collection.</p>
+      </div>
+    );
+  }    
